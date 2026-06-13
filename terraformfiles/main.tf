24386e58f51ad931140e1da1c6858eb78ba910e4
siddhesh.more <siddheshmore4848@gmail.com>
2026-06-13 10:31:42 +0000

    add resoruce

---
+++ terraformfiles/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg" {
+    name = "sid-rg"
+    location = "east us"
+  
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "sid-vnet-cicd-workshop"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name = "web-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
