c0b341ff042150232e63b57769bf21c4335ea903
siddhesh more <siddheshmore4848@gmail.com>
2026-06-20 11:31:56 +0000

    add subnet

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -17,3 +17,10 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.1.0.0/24"]
 }
+
+resource "azurerm_subnet" "app-subnet" {
+  name                 = "app-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
