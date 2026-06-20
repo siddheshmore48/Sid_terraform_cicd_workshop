98ed5865d023f87ebfffba23bd64999a290cdf1d
siddhesh more <siddheshmore4848@gmail.com>
2026-06-20 09:59:39 +0000

    change address space

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "sid-vnet-cicd-workshop"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 
@@ -15,5 +15,5 @@ resource "azurerm_subnet" "subnet" {
   name                 = "web-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.1.0.0/24"]
 }
